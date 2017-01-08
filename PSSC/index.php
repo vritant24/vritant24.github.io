f1968607a82a71a202c47d6be04910907b7c6974
vritant <vritoku@gmail.com>
2017-01-08 03:15:35 -0500

    added PSSC, the real one

--- PSSC/index.php
+++ PSSC/index.php
@@ -1,10 +1,4 @@
-<!-- <script src="https://code.jquery.com/jquery-3.1.1.min.js"
-integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-crossorigin="anonymous">
-$("#header").load("partials/header.html");
-$("#footer").load("partials/footer.html");
-</script> -->
-
 <?php include("partials/header.html") ?>
-  <h1>WELCOME</h1>
+<main>
+</main>
 <?php include("partials/footer.html") ?>
