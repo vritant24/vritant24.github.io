69267a49abc6dc1ac2e406e459d75b414e54818b
vritant <vritoku@gmail.com>
2017-01-08 03:14:29 -0500

    added PSSC

---
+++ PSSC/index.php
@@ -0,0 +1,10 @@
+<!-- <script src="https://code.jquery.com/jquery-3.1.1.min.js"
+integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+crossorigin="anonymous">
+$("#header").load("partials/header.html");
+$("#footer").load("partials/footer.html");
+</script> -->
+
+<?php include("partials/header.html") ?>
+  <h1>WELCOME</h1>
+<?php include("partials/footer.html") ?>
